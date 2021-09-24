447d9870b91a8ccd7b28465086b5c0ff27fdcad1
Santiago Cardona S <santicardonas777@gmail.com>
2021-09-23 21:49:12 -0500

    Create Post collage

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Body,
   Controller,
@@ -9,78 +10,47 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import * as Joi from 'joi';
-import Knex from 'knex';
-import { NestjsKnexService } from 'nestjs-knexjs';
-import { v4 as uuidv4 } from 'uuid';
-import * as bycript from 'bcrypt';
 
 const schema = Joi.object({
   name: Joi.string().required(),
-
   lastName: Joi.string().required(),
-
   email: Joi.string()
     .email({
       minDomainSegments: 2,
       tlds: { allow: ['com'] },
     })
     .required(),
-
   password: Joi.string()
-    .pattern(
-      new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'),
-    )
+    .pattern(new RegExp('^([a-zA-Z0-9!@#$%^&*]{6,16}$)'))
     .required(),
-
   gender: Joi.string().valid('male', 'female'),
-
   birthDate: Joi.string(),
 });
 
-@Controller('cities')
+@Controller('collage')
 export class CitiesController {
-  private readonly knex: Knex = null;
-
-  constructor(private nestjsKnexService: NestjsKnexService) {
-    this.knex = this.nestjsKnexService.getKnexConnection();
-  }
   @Get()
-  public async get(@Res() response: Response) {
-    const data = await this.knex('post').select('*');
-    Logger.log({ data });
-    return response.status(HttpStatus.OK).send({ data });
+  public get(@Res() response: Response) {
+    return response.status(HttpStatus.OK).send({});
   }
 
   @Post()
-  public async post(@Body() body: any, @Res() response: Response) {
+  public post(@Body() body: any, @Res() response: Response) {
     try {
       const result = schema.validate(body);
+      Logger.log({ result });
       if (result.error) {
         return response.status(HttpStatus.BAD_REQUEST).send({
-          error: result.error,
+          error: 'Invalid request body',
         });
       }
-      const password = body.password;
-      const saltRounds = 10;
-      const salt = bycript.genSaltSync(saltRounds);
-      const hashedPassword = bycript.hashSync(password, salt);
-      const id = uuidv4();
-      const data = {
-        id,
-        name: body.name,
-        lastName: body.lastName,
-        email: body.email,
-        password: hashedPassword,
-        gender: body.gender,
-        birthDate: body.birthDate,
+      const newUser = {
+        ...body,
+        creationDate: new Date().getTime(),
+        verifiedEmail: false,
       };
-      const table = await this.knex(' post ').insert(data);
-      Logger.log(table);
-      return response.status(HttpStatus.CREATED).send({ table });
-    } catch (ex) {
-      return response
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .send({ error: ex.message });
+      return response.status(HttpStatus.CREATED).send({ newUser });
+    } finally {
     }
   }
 }
